f1e22c3ca23058790a21c62159036536daaa46d4
Wagner <wagnerghedin10@gmail.com>
2021-10-14 21:53:04 -0300

    Criada tela de login

---
+++ app/src/main/java/com/example/guiaviagem/LoginPage.kt
@@ -0,0 +1,11 @@
+package com.example.guiaviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
